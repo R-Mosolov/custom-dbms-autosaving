dc970e2563f5da02a18296422fcf224146a1717e
R-Mosolov <R.V.Mosolov@ya.ru>
2023-01-26 11:22:00 +0300

    Create copy of databases

--- app.cjs
+++ app.cjs
@@ -1,4 +1,7 @@
 const http = require('node:http');
+const exec = require('child_process').exec;
+const { v4: uuidv4 } = require("uuid");
+const Docs = require("./dbms/Docs");
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,8 +13,22 @@ const server = http.createServer((req, res) => {
 });
 
 setInterval(() => {
+  const docs = new Docs();
+  docs.create("test-db", {
+    id: uuidv4(),
+    firstName: "Test",
+    email: "test@mail.ru",
+  });
+  exec(
+    "git add . && git commit -m \"Create copy of databases\" && git push",
+    function(err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(stdout);
+  });
   return console.log("The setInterval is finished");
-}, 1000);
+}, 5000);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
